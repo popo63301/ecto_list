2bb82a2e38e96f0999412c5e97bd6f9c24a3c203
popo63301 <popo63301@gmail.com>
2019-07-30 16:00:54 +0200

    list items functions

--- lib/ecto_list/list_item.ex
+++ lib/ecto_list/list_item.ex
@@ -79,4 +79,220 @@ defmodule EctoList.ListItem do
         insert_at(order_list, list_item, index)
     end
   end
+
+  @doc """
+  Move the list item id at the last position in the ordering.
+
+      iex> EctoList.ListItem.move_to_bottom([1, 2, 3, 4], 3)
+      [1, 2, 4, 3]
+
+      iex> EctoList.ListItem.move_to_bottom([1, 2, 3, 4], 1)
+      [2, 3, 4, 1]
+
+      iex> EctoList.ListItem.move_to_bottom([1, 2, 3, 4], 4)
+      [1, 2, 3, 4]
+
+      iex> EctoList.ListItem.move_to_bottom([1, 2, 3, 4], 5)
+      [1, 2, 3, 4, 5]
+
+  """
+  def move_to_bottom(order_list, list_item) do
+    length = length(order_list)
+
+    case Enum.member?(order_list, list_item) do
+      true -> insert_at(order_list, list_item, length)
+      false -> insert_at(order_list, list_item, length + 1)
+    end
+  end
+
+  @doc """
+  Move the list item id at the first position in the ordering.
+
+      iex> EctoList.ListItem.move_to_top([1, 2, 3, 4], 3)
+      [3, 1, 2, 4]
+
+      iex> EctoList.ListItem.move_to_top([1, 2, 3, 4], 1)
+      [1, 2, 3, 4]
+
+      iex> EctoList.ListItem.move_to_top([1, 2, 3, 4], 5)
+      [5, 1, 2, 3, 4]
+
+  """
+  def move_to_top(order_list, list_item) do
+    insert_at(order_list, list_item, 1)
+  end
+
+  @doc """
+  Remove the list item id in the ordering.
+
+      iex> EctoList.ListItem.remove_from_list([1, 2, 3, 4], 3)
+      [1, 2, 4]
+
+      iex> EctoList.ListItem.remove_from_list([1, 2, 3, 4], 1)
+      [2, 3, 4]
+
+      iex> EctoList.ListItem.remove_from_list([1, 2, 3, 4], 5)
+      [1, 2, 3, 4]
+
+  """
+  def remove_from_list(order_list, list_item) do
+    Enum.reject(order_list, &(&1 == list_item))
+  end
+
+  @doc """
+  Check if list item id is the first element in the ordering.
+
+      iex> EctoList.ListItem.first?([1, 2, 3, 4], 1)
+      true
+
+      iex> EctoList.ListItem.first?([1, 2, 3, 4], 3)
+      false
+
+      iex> EctoList.ListItem.first?([1, 2, 3, 4], 5)
+      false
+
+  """
+  def first?(order_list, list_item) do
+    List.first(order_list) == list_item
+  end
+
+  @doc """
+  Check if list item id is the last element in the ordering.
+
+      iex> EctoList.ListItem.last?([1, 2, 3, 4], 4)
+      true
+
+      iex> EctoList.ListItem.last?([1, 2, 3, 4], 2)
+      false
+
+      iex> EctoList.ListItem.last?([1, 2, 3, 4], 5)
+      false
+
+  """
+  def last?(order_list, list_item) do
+    List.last(order_list) == list_item
+  end
+
+  @doc """
+  Check if list item id is in the ordering.
+
+      iex> EctoList.ListItem.in_list?([1, 2, 3, 4], 3)
+      true
+
+      iex> EctoList.ListItem.in_list?([1, 2, 3, 4], 5)
+      false
+
+  """
+  def in_list?(order_list, list_item) do
+    Enum.member?(order_list, list_item)
+  end
+
+  @doc """
+  Check if list item id is in the ordering.
+
+      iex> EctoList.ListItem.not_in_list?([1, 2, 3, 4], 5)
+      true
+
+      iex> EctoList.ListItem.not_in_list?([1, 2, 3, 4], 3)
+      false
+  """
+  def not_in_list?(order_list, list_item) do
+    !Enum.member?(order_list, list_item)
+  end
+
+  @doc """
+  Return the list item id which is one rank higher in the ordering.
+
+      iex> EctoList.ListItem.higher_item([1, 7, 3, 4], 3)
+      7
+
+      iex> EctoList.ListItem.higher_item([1, 2, 3, 4], 1)
+      nil
+
+      iex> EctoList.ListItem.higher_item([1, 2, 3, 4], 5)
+      nil
+  """
+  def higher_item(order_list, list_item) do
+    index = Enum.find_index(order_list, &(&1 == list_item))
+
+    case index do
+      nil -> nil
+      0 -> nil
+      _ -> Enum.fetch!(order_list, index - 1)
+    end
+  end
+
+  @doc """
+  Return the list of ids above the list item id.
+
+      iex> EctoList.ListItem.higher_items([1, 2, 3, 4], 3)
+      [1, 2]
+
+      iex> EctoList.ListItem.higher_items([1, 2, 3, 4], 4)
+      [1, 2, 3]
+
+      iex> EctoList.ListItem.higher_items([1, 2, 3, 4], 1)
+      []
+
+      iex> EctoList.ListItem.higher_items([1, 2, 3, 4], 5)
+      nil
+  """
+  def higher_items(order_list, list_item) do
+    index = Enum.find_index(order_list, &(&1 == list_item))
+
+    case index do
+      nil -> nil
+      0 -> []
+      _ -> Enum.slice(order_list, 0, index)
+    end
+  end
+
+  @doc """
+  Return the list item id which is one rank lower in the ordering.
+
+      iex> EctoList.ListItem.lower_item([1, 2, 3, 7], 3)
+      7
+
+      iex> EctoList.ListItem.lower_item([1, 2, 3, 4], 4)
+      nil
+
+      iex> EctoList.ListItem.lower_item([1, 2, 3, 4], 5)
+      nil
+  """
+  def lower_item(order_list, list_item) do
+    index = Enum.find_index(order_list, &(&1 == list_item))
+    last_index = length(order_list) - 1
+
+    case index do
+      nil -> nil
+      ^last_index -> nil
+      _ -> Enum.fetch!(order_list, index + 1)
+    end
+  end
+
+  @doc """
+  Return the list of ids below the list item id.
+
+      iex> EctoList.ListItem.lower_items([1, 2, 3, 4], 2)
+      [3, 4]
+
+      iex> EctoList.ListItem.lower_items([1, 2, 3, 4], 1)
+      [2, 3, 4]
+
+      iex> EctoList.ListItem.lower_items([1, 2, 3, 4], 4)
+      []
+
+      iex> EctoList.ListItem.lower_items([1, 2, 3, 4], 5)
+      nil
+  """
+  def lower_items(order_list, list_item) do
+    index = Enum.find_index(order_list, &(&1 == list_item))
+    last_index = length(order_list) - 1
+
+    case index do
+      nil -> nil
+      ^last_index -> []
+      _ -> Enum.slice(order_list, index + 1, last_index - index)
+    end
+  end
 end
